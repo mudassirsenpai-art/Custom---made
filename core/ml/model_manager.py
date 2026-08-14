79d29f737dc9913a876daff45c6c63d144a6cd71
mudassirsenpai-art <mudassirsenpai@gmail.com>
2026-08-14 12:20:09 +0500

    Update model_manager.py

--- core/ml/model_manager.py
+++ core/ml/model_manager.py
@@ -169,7 +169,7 @@ class ModelManager:
                 "2x-AnimeSharpV4_Fast_RCAN_PU.safetensors"
             ),
             ModelType.LAMA: (
-                "https://huggingface.co/smartywu/big-lama/resolve/main/big-lama.pt"
+                "https://huggingface.co/fashn-ai/LaMa/resolve/main/big-lama.pt"
             ),
             ModelType.FLUX_KLEIN_SDCPP_VAE: (
                 "https://huggingface.co/Comfy-Org/flux2-dev/resolve/main/"
@@ -197,7 +197,7 @@ class ModelManager:
                 "filename": "2x-AnimeSharpV4_Fast_RCAN_PU.safetensors",
             },
             ModelType.LAMA: {
-                "repo_id": "smartywu/big-lama",
+                "repo_id": "fashn-ai/LaMa",
                 "filename": "big-lama.pt",
             },
             ModelType.YOLO_SPEECH_BUBBLE: {
