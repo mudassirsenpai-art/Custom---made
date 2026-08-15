50463e63536f5e88159f4cc5be1a33e5bd9541e4
mudassirsenpai-art <mudassirsenpai@gmail.com>
2026-08-15 09:07:10 +0500

    Update main.py

--- main.py
+++ main.py
@@ -887,7 +887,7 @@ def main():
     parser.add_argument(
         "--osb-inpainting-method",
         type=str,
-        choices=["flux_klein_9b", "flux_klein_4b", "flux_kontext", "lama", "opencv", "none"],
+        choices=["flux_klein_9b", "flux_klein_4b", "flux_kontext", "lama", "lama_large", "opencv", "none"],
         default="flux_klein_4b",
         help="Inpainting method for outside text removal.",
     )
